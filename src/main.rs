33bc568aa4496858e6d3236483e2ea2074fa9575
taroosg <root@taroosg.dev>
2024-10-02 22:04:33 +0900

    ヘルスチェック用のコードを追加

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,17 @@
-use std::net::{IpAddr, Ipv4Addr, SocketAddr};
-use axum::{routing::get, Router};
+use anyhow::Result;
+use axum::{http::StatusCode, routing::get, Router};
+use std::net::{Ipv4Addr, SocketAddr};
 use tokio::net::TcpListener;
 
-async fn hello_world() -> &'static str {
-    "Hello, World!"
+async fn health_check() -> StatusCode {
+    StatusCode::OK
 }
 
 #[tokio::main]
-async fn main() {
-    let app = Router::new().route("/hello", get(hello_world));
+async fn main() -> Result<()> {
+    let app = Router::new().route("/health", get(health_check));
     let addr = SocketAddr::new(Ipv4Addr::LOCALHOST.into(), 8080);
-    let listener = TcpListener::bind(addr).await.unwrap();
+    let listener = TcpListener::bind(addr).await?;
     println!("listening on {}", addr);
-    axum::serve(listener, app).await.unwrap();
+    Ok(axum::serve(listener, app).await?)
 }
