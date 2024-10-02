94c77adc950bb2b0ca732354f437bb297980f93b
taroosg <root@taroosg.dev>
2024-10-02 21:58:00 +0900

    サーバの動作を確認

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::{IpAddr, Ipv4Addr, SocketAddr};
+use axum::{routing::get, Router};
+use tokio::net::TcpListener;
+
+async fn hello_world() -> &'static str {
+    "Hello, World!"
+}
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/hello", get(hello_world));
+    let addr = SocketAddr::new(Ipv4Addr::LOCALHOST.into(), 8080);
+    let listener = TcpListener::bind(addr).await.unwrap();
+    println!("listening on {}", addr);
+    axum::serve(listener, app).await.unwrap();
 }
