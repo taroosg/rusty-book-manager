5691b66f09f763060234aa9eace69de0967ab228
taroosg <root@taroosg.dev>
2024-10-31 17:31:34 +0900

    apiにハンドラとルーターを定義

--- src/bin/app.rs
+++ src/bin/app.rs
@@ -1,6 +1,9 @@
 use adapter::database::connect_database_with;
 use anyhow::{Error, Result};
-use api::route::health::build_health_check_routers;
+use api::route::{
+    book::build_book_routers,
+    health::build_health_check_routers
+};
 use axum::Router;
 use registry::AppRegistry;
 use shared::config::AppConfig;
@@ -18,6 +21,7 @@ async fn bootstrap() -> Result<()> {
     let registry = AppRegistry::new(pool);
     let app = Router::new()
         .merge(build_health_check_routers())
+        .merge(build_book_routers())
         .with_state(registry);
     let addr = SocketAddr::new(Ipv4Addr::LOCALHOST.into(), 8080);
     let listener = TcpListener::bind(&addr).await?;
