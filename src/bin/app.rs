0900ffb65da14d70b63f7298acfd1f41e712e7b3
taroosg <root@taroosg.dev>
2024-11-02 11:50:29 +0900

    環境ごとのログ出力を追加

--- src/bin/app.rs
+++ src/bin/app.rs
@@ -7,10 +7,47 @@ use axum::Router;
 use registry::AppRegistry;
 use shared::config::AppConfig;
 use tokio::net::TcpListener;
+use tracing::{error, Subscriber};
+
+// ログ関連のもの
+use shared::env::{which, Environment};
+use tracing_subscriber::layer::SubscriberExt;
+use tracing_subscriber::util::SubscriberInitExt;
+use tracing_subscriber::EnvFilter;
+
+// httpリクエストとレスポンスに使用するもの
+use anyhow::Context;
+use tower_http::trace::{DefaultMakeSpan, DefaultOnRequest, DefaultOnResponse, TraceLayer};
+use tower_http::LatencyUnit;
+use tracing::Level;
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    init_logger()?;
     bootstrap().await
+}
+
+fn init_logger() -> Result<()> {
+    let log_level = match which() {
+        Environment::Development => "debug",
+        Environment::Production => "info",
+    };
+
+    // ログレベル
+    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| log_level.into());
+
+    // ログの出力形式
+    let subscriber = tracing_subscriber::fmt::layer()
+        .with_file(true)
+        .with_line_number(true)
+        .with_target(false);
+
+    tracing_subscriber::registry()
+        .with(subscriber)
+        .with(env_filter)
+        .try_init()?;
+
+    Ok(())
 }
 
 async fn bootstrap() -> Result<()> {
@@ -22,10 +59,31 @@ async fn bootstrap() -> Result<()> {
     let app = Router::new()
         .merge(build_health_check_routers())
         .merge(build_book_routers())
+        // .layer(cors())
+        .layer(
+            TraceLayer::new_for_http()
+                .make_span_with(DefaultMakeSpan::new().level(Level::INFO))
+                .on_request(DefaultOnRequest::new().level(Level::INFO))
+                .on_response(
+                    DefaultOnResponse::new()
+                        .level(Level::INFO)
+                        .latency_unit(LatencyUnit::Millis),
+                ),
+        )
         .with_state(registry);
 
     let addr = SocketAddr::new(Ipv4Addr::LOCALHOST.into(), 8080);
-    let listener = TcpListener::bind(addr).await?;
-    println!("Listening on {}", addr);
-    axum::serve(listener, app).await.map_err(Error::from)
+    let listener = tokio::net::TcpListener::bind(&addr).await?;
+    // println!("Listening on {}", addr);
+    tracing::info!("listening on {}", addr);
+    axum::serve(listener, app)
+        .await
+        .context("Unexpected error happened in server")
+        .inspect_err(|e| {
+            tracing::error!(
+                error.cause_chain = ?e,
+                error.message = %e,
+                "Unexpected error"
+            );
+        })
 }
