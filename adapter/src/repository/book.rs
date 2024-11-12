b0ca053f3b8c4ec65910d500eb215955413c512f
taroosg <root@taroosg.dev>
2024-11-12 19:32:02 +0900

    システム用にマイグレーションファイルを修正し，テストを実行しない状態に変更

--- adapter/src/repository/book.rs
+++ adapter/src/repository/book.rs
@@ -68,6 +68,7 @@ mod tests {
     use super::*;
 
     #[sqlx::test]
+    #[ignore]
     async fn test_register_book(pool: sqlx::PgPool) -> anyhow::Result<()> {
         let repo = BookRepositoryImpl::new(ConnectionPool::new(pool));
 
